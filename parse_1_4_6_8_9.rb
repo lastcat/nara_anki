17a9e9b30a015b3fa96e1b079c48623e363ec387
lastcat <kokodoko966@gmail.com>
2024-01-06 03:46:03 +0900

    save

--- parse_1_4_6_8_9.rb
+++ parse_1_4_6_8_9.rb
@@ -93,12 +93,245 @@ require 'pry'
             end
         end
     end
-    binding.irb
 
     questions.each do |q|
-        puts '問題: ' + q[0].to_s
-        puts q[1]
-        puts '解答: ' + q[2][a_answers[q[0]]]
+        #puts '問題: ' + q[0].to_s
+        #puts q[1]
+        #puts '解答: ' + q[2][a_answers[q[0]]]
+        if i == 1
+            if q[0] >= 1 && q[0] <= 11
+                File.open('shizen.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t気候、地理、動植物")
+                end
+            end
+            if q[0] >= 12 && q[0] <= 24
+                File.open('rekisi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t歴史")
+                end
+            end
+            if q[0] >= 25 && q[0] <= 40
+                File.open('iseki.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t遺跡、古墳")
+                end
+            end
+            if q[0] >= 41 && q[0] <= 59
+                File.open('shrine.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t寺社")
+                end
+            end
+            if q[0] >= 60 && q[0] <= 68
+                File.open('build.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t建築、彫刻、絵画")
+                end
+            end
+            if q[0] >= 69 && q[0] <= 76
+                File.open('bungaku.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t文学")
+                end
+            end
+            if q[0] >= 77 && q[0] <= 84
+                File.open('kougei.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t工芸、特産品")
+                end
+            end
+            if q[0] >= 85 && q[0] <= 92
+                File.open('maturi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t祭り、伝統行事")
+                end
+            end
+            if q[0] >= 93 && q[0] <= 100
+                File.open('kankou.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t観光")
+                end
+            end
+        end
+        if i == 4
+            if q[0] >= 1 && q[0] <= 9
+                File.open('shizen.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t気候、地理、動植物")
+                end
+            end
+            if q[0] >= 10 && q[0] <= 25
+                File.open('rekisi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t歴史")
+                end
+            end
+            if q[0] >= 26 && q[0] <= 41
+                File.open('iseki.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t遺跡、古墳")
+                end
+            end
+            if q[0] >= 42 && q[0] <= 59
+                File.open('shrine.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t寺社")
+                end
+            end
+            if q[0] >= 60 && q[0] <= 69
+                File.open('build.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t建築、彫刻、絵画")
+                end
+            end
+            if q[0] >= 70 && q[0] <= 78
+                File.open('bungaku.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t文学")
+                end
+            end
+            if q[0] >= 79 && q[0] <= 87
+                File.open('kougei.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t工芸、特産品")
+                end
+            end
+            if q[0] >= 88 && q[0] <= 93
+                File.open('maturi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t祭り、伝統行事")
+                end
+            end
+            if q[0] >= 94 && q[0] <= 100
+                File.open('kankou.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t観光")
+                end
+            end
+        end
+        if i == 6
+            if q[0] >= 1 && q[0] <= 10
+                File.open('shizen.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t気候、地理、動植物")
+                end
+            end
+            if q[0] >= 11 && q[0] <= 21
+                File.open('rekisi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t歴史")
+                end
+            end
+            if q[0] >= 22 && q[0] <= 32
+                File.open('iseki.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t遺跡、古墳")
+                end
+            end
+            if q[0] >= 33 && q[0] <= 50
+                File.open('shrine.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t寺社")
+                end
+            end
+            if q[0] >= 51 && q[0] <= 60
+                File.open('build.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t建築、彫刻、絵画")
+                end
+            end
+            if q[0] >= 61 && q[0] <= 70
+                File.open('bungaku.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t文学")
+                end
+            end
+            if q[0] >= 71 && q[0] <= 80
+                File.open('kougei.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t工芸、特産品")
+                end
+            end
+            if q[0] >= 81 && q[0] <= 90
+                File.open('maturi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t祭り、伝統行事")
+                end
+            end
+            if q[0] >= 91 && q[0] <= 100
+                File.open('kankou.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t観光")
+                end
+            end
+        end
+        if i == 8
+            if q[0] >= 1 && q[0] <= 10
+                File.open('shizen.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t気候、地理、動植物")
+                end
+            end
+            if q[0] >= 11 && q[0] <= 20
+                File.open('rekisi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t歴史")
+                end
+            end
+            if q[0] >= 21 && q[0] <= 31
+                File.open('iseki.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t遺跡、古墳")
+                end
+            end
+            if q[0] >= 32 && q[0] <= 49
+                File.open('shrine.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t寺社")
+                end
+            end
+            if q[0] >= 50 && q[0] <= 57
+                File.open('build.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t建築、彫刻、絵画")
+                end
+            end
+            if q[0] >= 58 && q[0] <= 68
+                File.open('bungaku.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t文学")
+                end
+            end
+            if q[0] >= 69 && q[0] <= 78
+                File.open('kougei.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t工芸、特産品")
+                end
+            end
+            if q[0] >= 79 && q[0] <= 89
+                File.open('maturi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t祭り、伝統行事")
+                end
+            end
+            if q[0] >= 90 && q[0] <= 100
+                File.open('kankou.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t観光")
+                end
+            end
+        end
+        if i == 9
+            if q[0] >= 1 && q[0] <= 10
+                File.open('shizen.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t気候、地理、動植物")
+                end
+            end
+            if q[0] >= 11 && q[0] <= 21
+                File.open('rekisi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t歴史")
+                end
+            end
+            if q[0] >= 22 && q[0] <= 32
+                File.open('iseki.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t遺跡、古墳")
+                end
+            end
+            if q[0] >= 33 && q[0] <= 51
+                File.open('shrine.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t寺社")
+                end
+            end
+            if q[0] >= 52 && q[0] <= 60
+                File.open('build.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t建築、彫刻、絵画")
+                end
+            end
+            if q[0] >= 61 && q[0] <= 70
+                File.open('bungaku.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t文学")
+                end
+            end
+            if q[0] >= 71 && q[0] <= 80
+                File.open('kougei.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t工芸、特産品")
+                end
+            end
+            if q[0] >= 81 && q[0] <= 90
+                File.open('maturi.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t祭り、伝統行事")
+                end
+            end
+            if q[0] >= 90 && q[0] <= 100
+                File.open('kankou.txt', 'a') do |f|
+                    f.puts("#{q[1]}\t#{q[2][a_answers[q[0]]]}\t観光")
+                end
+            end
+        end
     end
-
 end
